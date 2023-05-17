296fa2225fd5b2e705efa637406c06df5ae0a04b
AyimWilliam <odhisayim@gmail.com>
2023-05-18 01:48:47 +0300

    finished function sum

---
+++ DAY-4/sum.py
@@ -0,0 +1,9 @@
+numbers =[12,45,23,78,28,47]
+
+def calculate_sum(numbers):
+    total_sum = sum(numbers)
+    print (f"The sum of the numbers is : {total_sum}") 
+
+
+actual_sum = calculate_sum(numbers)
+print(actual_sum)
